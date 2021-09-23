8d125cf0ad3460af978fddb5f6911ebee83a8d64
geraldinemulimi <geraldinemulimi@gmail.com>
2021-09-23 18:24:03 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import datetime as dt
+import pandas
+import random
+import smtplib
+
+my_EMAIL = "learningpython29@gmail.com"
+my_PASSWORD = "data2021"
+
+
+now = dt.datetime.now()
+month = now.month
+day = now.day
+today = (month, day)
+
+
+data = pandas.read_csv("birthdays.csv")
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+
+if today in birthday_dict:
+    birthday_person = birthday_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_EMAIL, password=my_PASSWORD)
+        connection.sendmail(from_addr=my_EMAIL,
+                            to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy Birthday\n\n{contents}")
+
+
+
+
+# 4. Send the letter generated in step 3 to that person's email address.
+# HINT 1: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+# HINT 2: Remember to call .starttls()
+# HINT 3: Remember to login to your email service with email/password. Make sure your security setting is set to allow less secure apps.
+# HINT 4: The message should have the Subject: Happy Birthday then after \n\n The Message Body.
+
+
+
